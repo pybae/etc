1d974040a878528ad1c05489ce9dc7c579350392
Paul Bae <pbae@utexas.edu>
2014-08-28 17:59:18 -0500

    Added problem 3 of Project Euler

---
+++ Project Euler/1-10/problem3.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600851475143 ?
+
+def sieve_of_erathosthenes(n):
+    factors = set()
+    primes = []
+    for i in range(2, n+1):
+        if i not in factors:
+            primes.append(i)
+            factors.update(range(i*i, n+1, i))
+    return primes
+
+n = 600851475143 
+i = 0
+max_factor = 2 # assuming n >= 2
+
+primes = sieve_of_erathosthenes(100000)
+while n > 1:
+    if n % primes[i] == 0:
+    	n /= primes[i]
+    else:
+    	i += 1
+print primes[i]
