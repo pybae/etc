31b3f4348325aca736e80871c5ecb6a64eff89e0
Paul Bae <pbae@utexas.edu>
2014-09-01 11:10:38 -0500

    added pol_17

---
+++ Stack Overflow/pol_17.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+datafilename = 'pol_17.txt'
+g = np.genfromtxt(datafilename, skiprows=6, usecols=(1, 2, 3, 4, 5, 6,7), 
+        delimiter=None, dtype=[('f0', '<i4'), ('f1', '<i4'), ('f2', '<i4'),
+            ('f3', '<f8'), ('f4', '<f8'), ('f5', '<f8'), ('f6', '<f8')])
+print g
+print g['f0']
+
+for idx, num in enumerate(g['f0']):
+    if num == 17:
+    	print g[idx]
