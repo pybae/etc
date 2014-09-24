e767479d2120e3eb1f81c036e66cc8d5ca128ba6
Paul Bae <pbae@utexas.edu>
2014-09-24 16:23:18 -0500

    Added phpunit testing framework
    First try!

---
+++ Project Euler/1-9/php/app/autoload.php
@@ -0,0 +1,7 @@
+<?php
+
+function __autoload($class_name) {
+  include $class_name . '.php';
+}
+
+__autoload("Solutions");
---
+++ Project Euler/1-9/php/tests/TestSolutions.php
@@ -0,0 +1,13 @@
+<?php
+
+class SolutionsTest extends PHPUnit_Framework_TestCase
+{
+  /**
+   * @small
+   */
+  public function testProblem1()
+  {
+    $s = new Solutions();
+    $this->assertEquals(233168, $s->problem1());
+  }
+}
