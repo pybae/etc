312bf6352b745fb569d5373a188120cd208fb6bc
Paul Bae <pbae@utexas.edu>
2014-09-30 00:16:20 -0500

    Done with first 10

--- Project Euler/1-9/php/app/Solutions.php
+++ Project Euler/1-9/php/app/Solutions.php
@@ -208,6 +208,17 @@ class Solutions
 
     function Problem9()
     {
-        return 0;
+        for ($m = 1; $m < 10; $m++) {
+            for ($n = 1; $n < $m; $n++) {
+                $a = pow($m, 2) - pow($n, 2);
+                $b = 2*$m*$n;
+                $c = pow($m, 2) + pow($n, 2);
+                if (1000 % ($a+$b+$c) === 0) {
+                    $factor = 1000 / ($a+$b+$c);
+                    return pow($factor, 3) * $a * $b * $c;
+                }
+            }
+        }
+        return 0; // dummy return
     }
 }
