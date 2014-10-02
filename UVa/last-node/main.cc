3705d52514d3cb86d8658e9b6b5c197df69ca88b
Paul Bae <pbae@utexas.edu>
2014-10-02 12:14:10 -0500

    done with lastnode

--- UVa/last-node/main.cc
+++ UVa/last-node/main.cc
@@ -7,44 +7,61 @@
 
 using namespace std;
 
-// Does a eval on the nodes
 int eval(int startNode, int limit, map<int, vector<int> > edges)
 {
-  set<int> visited;
-  for (int i = 0; i < edges.size(); i++) {
-    for (int j = 0; j < edges[i].size(); j++) {
-      cout << edges[i][j] << endl;
+    map<int, int> visited;
+    queue<int> s;
+    
+    s.push(startNode);
+    visited[startNode]=0;
+    
+    while (!s.empty()) {
+        int top = s.front();
+        s.pop();
+        int size = edges[top].size();
+        for (int i = 0; i < size; i++) {
+            int n = edges[top][i];
+            if (!visited.count(n)) { // Already been here
+                visited[n] = visited[top] + 1;
+                s.push(n);
+            }
+        }
     }
-  }
+    
+    map <int,int>::const_iterator itr;
+    int result = 0;
+    for(itr = visited.begin(); itr != visited.end(); ++itr){
+        if ((*itr).second>limit) {
+            result++;
+        }
+   }
+   result += edges.size()-visited.size(); //Aaaaaah!!! The graph could has no connected nodes 
 
-
-  return 0;
+   return result;
 }
 
 int main(int argc, const char *argv[])
 {
-  int numNodes = 0;
-  while (cin >> numNodes && numNodes) {
-    printf("%d\n", numNodes);
-
-    map <int, vector<int> > edges;
-    for (int i = 0; i < numNodes; i++) {
-      int a, b;
-      cin >> a;
-      cin >> b;
-      edges[a].push_back(b);
-      edges[b].push_back(a);
-    }
+    int numNodes = 0;
+    while (cin >> numNodes && numNodes) {
+        map <int, vector<int> > edges;
+        for (int i = 0; i < numNodes; i++) {
+            int a, b;
+            cin >> a;
+            cin >> b;
+            edges[a].push_back(b);
+            edges[b].push_back(a);
+        }
     
-    int startNode, limit;
-    int numCases;
-    while ((cin >> startNode && cin >> limit) &&
-           (startNode != 0 && limit != 0)) { 
-      numCases++;
-      int result = eval(startNode, limit, edges);
-      cout << "Case 1 " << numCases << ": " << result \
-        << " nodes not reachable from node " << startNode << " with TTL = " << limit;
+        int startNode, limit;
+        int numCases;
+        while ((cin >> startNode && cin >> limit) &&
+               (startNode != 0 && limit != 0)) { 
+            numCases++;
+            int result = eval(startNode, limit, edges);
+            cout << "Case " << numCases << ": " << result \
+                 << " nodes not reachable from node " << startNode << " with TTL = " << limit << endl;
+        }
     }
-  }
-  return 0;
+    return 0;
 }
