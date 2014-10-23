856ad7e6be33849c6dba46efd13f30e1c3e11d9f
Paul Bae <pbae@utexas.edu>
2014-10-22 23:15:11 -0500

    updated test code for otsu

--- OpenCV/otsu.cc
+++ OpenCV/otsu.cc
@@ -9,11 +9,16 @@ using namespace cv;
 
 int main(int argc, const char *argv[])
 {
-    if (argc > 1) {
-        Mat im = imread(argv[1], 1);
+    if (argc != 2) {
+        std::cout << "Please specify one filename" << std::endl;
+        return -1;
     }
-    else {
-        std::cout << "Please specify a filename" << std::endl;
-    }
+    
+    Mat im = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+    Mat output;
+    threshold(im, output, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
+
+    imshow("Threshold: ", output);
+    waitKey(0);
     return 0;
 }
